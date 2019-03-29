d8fe99ea213a53e381cbf97a2508473fb020714e
Sebastian Velasquez <sebastian.v.a@riseup.net>
2019-03-29 17:29:42 -0300

    simple modifications to make it work

--- node-login/models/user.js
+++ node-login/models/user.js
@@ -4,18 +4,25 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const userSchema = mongoose.Schema({ 
+const userSchema = mongoose.Schema({
 
 	name 			: String,
-	email			: {type: String, unique: true}, 
+	email			: {type: String, unique: true},
 	hashed_password	: String,
 	created_at		: String,
 	temp_password	: String,
 	temp_password_time: String
-	
+
 });
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/node-login');
 
-module.exports = mongoose.model('user', userSchema);        
+mongoose.connect('mongodb+srv://dada:@cluster0-7ffvo.mongodb.net/test?retryWrites=true',
+ {
+   useNewUrlParser: true
+ }).then( () => {
+    console.log('Connection to the Atlas Cluster is successful!')
+  })
+  .catch( (err) => console.error(err));
+
+module.exports = mongoose.model('user', userSchema);
